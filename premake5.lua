9035e3cde2e4c0ba939bbaed6139d5b773a476ad
Drew Funderburk <kevinfunderburkbusiness@gmail.com>
2021-09-03 17:29:42 -0500

    Implemented premake
    Set up all defines
    Post build dll copy to Sandbox

---
+++ premake5.lua
@@ -0,0 +1,106 @@
+workspace "Launch"
+	architecture "x64"
+	startproject "Sandbox"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Launch"
+	location "Launch"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"LN_PLATFORM_WINDOWS",
+			"LN_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "LN_DEBUG"
+		symbols "On"
+		
+	filter "configurations:Release"
+		defines "LN_Release"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "LN_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Launch/vendor/spdlog/include",
+		"Launch/src"
+	}
+
+	links
+	{
+		"Launch"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"LN_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "LN_DEBUG"
+		symbols "On"
+		
+	filter "configurations:Release"
+		defines "LN_Release"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "LN_DIST"
+		optimize "On"
