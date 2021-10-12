b9a9d250b6034d201765652b899462fd23fc3a70
Drew Funderburk <kevinfunderburkbusiness@gmail.com>
2021-10-11 21:23:28 -0500

    Sandbox Tests

--- Sandbox/src/Sandbox.cpp
+++ Sandbox/src/Sandbox.cpp
@@ -5,7 +5,12 @@ class Sandbox : public Launch::Application
 public:
 	Sandbox()
 	{
-
+		Launch::Vector4 test4 = Launch::Vector4();
+		LN_TRACE(test4.toString());
+		Launch::Vector3 test3 = Launch::Vector3();
+		LN_TRACE(test3.toString());
+		Launch::Vector2 test2 = Launch::Vector2();
+		LN_TRACE(test2.toString());
 	}
 	~Sandbox()
 	{
